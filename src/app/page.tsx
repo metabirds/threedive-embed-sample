f6c2f835b7183b22904509787d6f2b05a112e93a
metakat <metakatz@gmail.com>
2025-11-26 19:27:22 +0900

    add title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Script from "next/script";
 export default function Home() {
   return (
     <>
+      <h1>ThreeDive Embed Sample</h1>
       <div id="td-zU6Oi4Z7BA"></div>
       <Script src="http://localhost:5173/embed.js?s=zU6Oi4Z7BA" />
     </>
