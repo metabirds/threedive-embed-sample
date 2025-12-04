8550cac42690f7be1b5e5c4fe8ba6816f6a01dfd
metakat <metakatz@gmail.com>
2025-12-04 15:41:19 +0900

    change sample id

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ export default function Home() {
   return (
     <>
       <h1>ThreeDive Embed Sample</h1>
-      <div id="td-0rrwGOKqRf" data-width="100%"  data-parent-url="https://metabirds.github.io/threedive-embed-sample"></div>
+      <div id="td-0WtPm9cA1J" data-width="100%"  data-parent-url="https://metabirds.github.io/threedive-embed-sample"></div>
       {/* using next/script to load embed.js for working with next.js */}
-      <Script src="https://app.threedive.ai/embed.js?s=0rrwGOKqRf"></Script>
+      <Script src="https://app.threedive.ai/embed.js?s=0WtPm9cA1J"></Script>
     </>
   );
 }
